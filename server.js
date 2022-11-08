01e3faaff591db3207b364089d5646d5bbcc5561
tejasethinos <tejas.kesaria@ethinos.com>
2022-11-08 17:00:47 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ const sequelize = require("./sequelize");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
-app.use(
-  cors({
+app.use(cors({
     origin: "*",
   })
 );
